0ad9c5becf239d6cc2931a09b3ab39cfea526e31
pinto921 <pinto921@hotmail.com>
2016-10-28 07:34:49 +0000

    task 6

--- task_06.py
+++ task_06.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Lesson 07, Task 05"""
+""" complete the code"""
 
 
 import data
 
 SUPER_SIDEKICKS = {}
 for HERO, HERO_DATA in data.SUPERHEROES.iteritems():
-    SUPER_SIDEKICKS[HERO] = # Complete this line
+    SUPER_SIDEKICKS[HERO] = #complete this line
+    SUPER_SIDEKICKS[HERO] = HERO_DATA.get('pet')
